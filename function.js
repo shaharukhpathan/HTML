879902b69b98d894bc3d2e6b11e9a3c2c9995365
shaharukhpathan <89965634+shaharukhpathan@users.noreply.github.com>
2021-09-14 18:10:11 +0530

    vfunc

---
+++ function.js
@@ -0,0 +1,20 @@
+// function fullName(fName, lName){
+//     console.log( fName+" "+ lName);
+// }
+// fullName("FARHAN", "AKHTAR");
+// -----------------fullName optimum-----------
+// *------------------------------/
+function fullName(fName, lName){
+  return fName+" "+ lName;
+}
+console.log(fullName("FARHAN", "AKHTAR"));
+
+// *------------------------------------
+
+
+function fullName(fName, lName){
+    return fName+" "+ lName;
+}
+var FULLNAME= fullName("FARHAN", "AKHTAR");
+console.log(FULLNAME);
+
