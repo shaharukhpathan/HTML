2fa40ff769a884fa5f0b2b57aae115411f040d4d
shaharukhpathan <89965634+shaharukhpathan@users.noreply.github.com>
2021-09-15 18:04:16 +0530

    check conditiion
    i am done the task

---
+++ areaofreacc.js
@@ -0,0 +1,2 @@
+const areaofRect=(l,b)=>l*b;
+console.log(areaofRect(30,40));
---
+++ sumnum.js
@@ -0,0 +1,9 @@
+// const sumnum=(num1,num2)=>{
+//     return num1+num2;
+// };
+// console.log(sumnum(25,152));
+
+
+// ------second method-----//
+const sumnum=(num1,num2)=> num1+num2;
+console.log(sumnum(25,152));
